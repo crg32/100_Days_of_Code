f8bd461c0f932d242987e4add1c0268406875057
court <courtrgriffin@gmail.com>
2022-01-09 20:38:37 -0500

    Starting day 16 work, using turtle methods

---
+++ day16-start/main.py
@@ -0,0 +1,14 @@
+import turtle
+import another_module
+
+print(another_module.another_variable)
+
+tinny = turtle.Turtle()
+print(tinny)
+tinny.shape("turtle")
+tinny.color("CadetBlue4")
+tinny.forward(100)
+
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
